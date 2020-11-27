28bc76a33fa2b76a28ee1506660818395586951b
J Rhee <jrhee@Justins-MacBook-Pro.local>
2020-11-27 18:54:12 -0500

    changed file name to public, it was given on hw page, worked on the requirements for htmlroutes, and added the basic dependecies for apirouter

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+// DEPENDENCIES
+const path = require("path");
+
+module.exports = function(app) {
+
+//GET `/notes` - Should return the `notes.html` file.
+//GET `*` - Should return the `index.html` file
+
+    app.get("notes", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+};
