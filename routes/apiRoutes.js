457201268e566dd3aa0f04c7382b22bf6ee91840
J Rhee <jrhee@Justins-MacBook-Pro.local>
2020-11-28 19:34:52 -0500

    added an app.get for the html and worked on some stuff for the apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+let noteData = require("../db/db.json")
 
 module.exports = function(app){
-    
+    var data = fs.readFileSync("./db/db.json", "utf8");
+
+    app.get("/api/notes", function (req, res){
+        res.json(noteData);
+    });
+    app.post("/api/notes", function (req,res){
+        let newNote = req.body;
+        newNote.id = Math.floor(Math.random() * 10000);
+
+        noteData.push(newNote);
+        res.json(noteData);
+
+        fs.writeFile("./db/db.json", JSON.stringify(noteData), function (err){
+            if (err) throw err;
+            res.end(noteData);
+            console.log(success);
+        });
+    });
+    app.delete("/api/notes/:id", function(req, res){
+        
+    })
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,6 +10,10 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    })
+
     app.get("*", function(req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
