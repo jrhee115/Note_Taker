20463ed028c275a22b5e6faa3fb214f0ac731d3e
J Rhee <jayrhee0514@gmail.com>
2020-12-01 14:34:29 -0500

    changed up app.post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,9 @@
 var noteData = require("../db/db.json");
 var fs = require("fs");
+var util = require ("util");
+var string = require("string")
+
+var writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = function(app){
 
@@ -7,11 +11,19 @@ module.exports = function(app){
         res.json(noteData);
     });
     app.post("/api/notes", function (req,res){
-        var newNote = req.body;
-        noteData.push(newNote);
-        res.json(noteData);
+       req.body.id = string();
+       noteData.push(req.body)
+
+       try{
+           await writeFileAsync("./db/db.json", JSON.stringify(noteData));
+           console.log("updated");
+           return res.json(true);
+       }
+       catch(err){
+           console.log(err);
+           return res.json(false);
+       }
     });
     app.delete("/api/notes/:id", function(req, res){
-        
-    })
+    });
 }
